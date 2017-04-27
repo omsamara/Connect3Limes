792409905db0724eb4507940146854a8add8bcd3
Omar Samara <omsamara@ncsu.edu>
2017-04-27 11:18:08 -0400

    Initial commit

---
+++ app/src/main/java/com/connect3/bman/connect3limes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.connect3.bman.connect3limes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
