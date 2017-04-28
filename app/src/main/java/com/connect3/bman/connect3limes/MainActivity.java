6785d53750e4ac00d65970749d4a951e9b886ccc
Omar Samara <omsamara@ncsu.edu>
2017-04-28 10:17:47 -0400

    Added in countdown timer to delay CPU's move until after the player has finished

--- app/src/main/java/com/connect3/bman/connect3limes/MainActivity.java
+++ app/src/main/java/com/connect3/bman/connect3limes/MainActivity.java
@@ -1,17 +1,30 @@
 package com.connect3.bman.connect3limes;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import static com.connect3.bman.connect3limes.R.id.sapce0;
+
 public class MainActivity extends AppCompatActivity {
 
     /** 0 means it is the player's turn. 1 means it is the CPU's turn */
-    private int playerTurn;
+    private int currTurn;
 
     /** Array of ints representing the game's state. 2 means no player has picked this spot */
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    ImageView space0;
+    ImageView space1;
+    ImageView space2;
+    ImageView space3;
+    ImageView space4;
+    ImageView space5;
+    ImageView space6;
+    ImageView space7;
+    ImageView space8;
 
     /**
      * Method that runs when the app is launched.
@@ -22,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playerTurn = 0;
+        currTurn = 0;
     }
 
     /**
@@ -36,25 +49,34 @@ public class MainActivity extends AppCompatActivity {
         int gridPos = Integer.parseInt((String)counter.getTag());
         if (gameState[gridPos] == 2) {
             counter.setTranslationY(-1000f);
-
-            if (playerTurn == 0) {
-                gameState[gridPos] = 0;
-                counter.setImageResource(R.drawable.lime);
-                playerTurn = 1;
-            }
-            else if (playerTurn == 1){
-                gameState[gridPos] = 1;
-                counter.setImageResource(R.drawable.lemon);
-                playerTurn = 0;
-            }
-
+            gameState[gridPos] = 0;
+            counter.setImageResource(R.drawable.lime);
             counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+            new CountDownTimer(400, 100) {
+
+                public void onTick(long millisUntilFinished) {
+                    //do nothing
+                }
+                public void onFinish() {
+                    takeCPUTurn();
+                }
+            }.start();
+
+
         }
         else {
-            return;
+          return;
         }
 
 
 
+    }
+
+    public void takeCPUTurn() {
+        space0 = (ImageView) findViewById(R.id.sapce0);
+        space0.setTranslationY(-1000f);
+        gameState[0] = 1;
+        space0.setImageResource(R.drawable.lemon);
+        space0.animate().translationYBy(1000f).rotation(360).setDuration(300);
     }
 }
